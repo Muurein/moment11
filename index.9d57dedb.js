32b951efc27f364d55889181ed8b6108e6e3744d
Muurein <amanda.l.thim@gmail.com>
2025-01-30 22:12:38 +0100

    Updates

---
+++ index.9d57dedb.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector(".hamburger-menu"),t=document.querySelector(".menu-content");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active")}),document.querySelectorAll(".menu-link").forEach(c=>c.addEventListener("click",()=>{e.classList.remove("active"),t.classList.remove("active")}))}();
+//# sourceMappingURL=index.9d57dedb.js.map
---
+++ index.bdb3a0f8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".hamburger-menu"),t=document.querySelector(".menu-content");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active")}),document.querySelectorAll(".menu-link").forEach(c=>c.addEventListener("click",()=>{e.classList.remove("active"),t.classList.remove("active")}));
+//# sourceMappingURL=index.bdb3a0f8.js.map
